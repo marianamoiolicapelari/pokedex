8b29ba4fb717d729dc5f929945dbdf74c799a2f2
Mariana M.C <marianamoioli@gmail.com>
2022-10-28 09:33:11 -0300

    added new features

--- scripts.js
+++ scripts.js
@@ -11,6 +11,9 @@ const pokemonXp = document.querySelector('.xp')
 const pokemonHabilities = document.querySelector('.habilities')
 
 const btnBulbasaur = document.getElementById('btn-bulbasaur')
+const btnCharmander = document.getElementById('btn-charmander')
+const btnDragonite = document.getElementById('btn-dragonite')
+const btnGyarados = document.getElementById('btn-gyarados')
 const btnPikachu = document.getElementById('btn-pikachu')
 
 const fetchPokemon = async (pokemon) => {
@@ -31,15 +34,41 @@ const renderBulbasaur = async (pokemon) => {
   card.style.background = 'linear-gradient(to top, #fff8bf 0% 50%, #59BC38 50% 100%)'
 }
 
+const renderCharmander = async (pokemon) => {
+  const data = await fetchPokemon(pokemon)
 
-
-function bulba(){
-  renderBulbasaur(1)
+  pokemonName.innerHTML = data.name
+  pokemonId.innerHTML = data.id
+  pokemonType.innerHTML = data.types[0].type.name
+  pokemonXp.innerHTML = `XP: ${data.base_experience}`
+  pokemonHabilities.innerHTML = data.abilities[1].ability.name
+  cardImg.src = "./assets/charmander.png"
+  card.style.background = 'linear-gradient(to top, #fff8bf 0% 50%, #F68C00 50% 100%)'
 }
 
-btnBulbasaur.addEventListener("click", bulba)
+const renderDragonite = async (pokemon) => {
+  const data = await fetchPokemon(pokemon)
 
+  pokemonName.innerHTML = data.name
+  pokemonId.innerHTML = data.id
+  pokemonType.innerHTML = data.types[0].type.name
+  pokemonXp.innerHTML = `XP: ${data.base_experience}`
+  pokemonHabilities.innerHTML = data.abilities[1].ability.name
+  cardImg.src = "./assets/dragonite.png"
+  card.style.background = 'linear-gradient(to top, #fff8bf 0% 50%, #FABE00 50% 100%)'
+}
 
+const renderGyarados = async (pokemon) => {
+  const data = await fetchPokemon(pokemon)
+
+  pokemonName.innerHTML = data.name
+  pokemonId.innerHTML = data.id
+  pokemonType.innerHTML = data.types[0].type.name
+  pokemonXp.innerHTML = `XP: ${data.base_experience}`
+  pokemonHabilities.innerHTML = data.abilities[1].ability.name
+  cardImg.src = "./assets/gyarados.png"
+  card.style.background = 'linear-gradient(to top, #fff8bf 0% 50%, #3DD4FB 50% 100%)'
+}
 
 const renderPikachu = async (pokemon) => {
   const data = await fetchPokemon(pokemon)
@@ -53,19 +82,16 @@ const renderPikachu = async (pokemon) => {
   card.style.background = 'linear-gradient(to top, #fff8bf 0% 50%, #FFFC22 50% 100%)'
 }
 
+btnBulbasaur.addEventListener("click", () => renderBulbasaur(1))
+btnCharmander.addEventListener("click", () => renderCharmander(4))
+btnDragonite.addEventListener("click", () => renderDragonite(149))
+btnGyarados.addEventListener("click", () => renderGyarados(22))
 btnPikachu.addEventListener("click", () => renderPikachu(25))
 
 
-// btnBulbasaur.addEventListener("click", renderBulbasaur (1) {
-//   cardBack.style.display === 'none' ? cardPrincipal.style.display = 'block' : cardBack.style.display = 'none'
-// })
+
+btnBulbasaur.addEventListener("click", function() {
+  cardBack.style.display === 'none' ? cardPrincipal.style.display = 'block' : cardBack.style.display = 'none'
+})
 
 
-/*
-  card.style.background = 'linear-gradient(to top, #fff8bf 0% 50%, #3DD4FB 50% 100%)
-  card.style.background = 'linear-gradient(to top, #fff8bf 0% 50%, #FFFC22 50% 100%)
-  card.style.background = 'linear-gradient(to top, #fff8bf 0% 50%, #A28CDE 50% 100%)
-  card.style.background = 'linear-gradient(to top, #fff8bf 0% 50%, #FABE00 50% 100%)
-  card.style.background = 'linear-gradient(to top, #fff8bf 0% 50%, #F68C00 50% 100%)
-  card.style.background = 'linear-gradient(to top, #fff8bf 0% 50%, #59BC38 50% 100%)
-*/

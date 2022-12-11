422b1a42afd4b910d5c2acc1073d47a9a1ccea1a
Mariana M.C <marianamoioli@gmail.com>
2022-12-11 09:36:38 -0300

    added new functions

--- scripts.js
+++ scripts.js
@@ -1,10 +1,8 @@
-// const card = document.querySelector('.card')
-
 const pokemonName = document.querySelector('.pokemon-name')
 const pokemonId = document.querySelector('.pokemon-id')
 const pokemonType = document.querySelector('.type')
 const cardImg = document.querySelector('.card-img')
-const card = document.querySelector('.card')
+const card = document.getElementById('card-bg')
 const cardPrincipal = document.querySelector('.card-principal')
 const cardBack = document.querySelector('.card-back')
 const pokemonXp = document.querySelector('.xp')
@@ -22,92 +20,56 @@ const fetchPokemon = async (pokemon) => {
   return data
 }
 
-const renderBulbasaur = async (pokemon) => {
-  const data = await fetchPokemon(pokemon)
+const renderPokemon = async (id, imagem, background) => {
+  const pokemon = await fetchPokemon(id)
 
-  pokemonName.innerHTML = data.name
-  pokemonId.innerHTML = data.id
-  pokemonType.innerHTML = data.types[0].type.name
-  pokemonXp.innerHTML = `XP: ${data.base_experience}`
-  pokemonHabilities.innerHTML = data.abilities[1].ability.name
-  cardImg.src = "./assets/bulbasaur.png"
-  card.style.background = 'linear-gradient(to top, #fff8bf 0% 50%, #59BC38 50% 100%)'
+  pokemonName.innerHTML = pokemon.name
+  pokemonId.innerHTML = pokemon.id
+  pokemonType.innerHTML = pokemon.types[0].type.name
+  pokemonXp.innerHTML = `XP: ${pokemon.base_experience}`
+  pokemonHabilities.innerHTML = pokemon.abilities[1].ability.name
+  cardImg.src = `./assets/${imagem}`
+  card.setAttribute('style',`background: linear-gradient(to top,  #fff8bf 0% 50%, ${background} 50% 100%) !important`) 
 }
 
-const renderCharmander = async (pokemon) => {
-  const data = await fetchPokemon(pokemon)
 
-  pokemonName.innerHTML = data.name
-  pokemonId.innerHTML = data.id
-  pokemonType.innerHTML = data.types[0].type.name
-  pokemonXp.innerHTML = `XP: ${data.base_experience}`
-  pokemonHabilities.innerHTML = data.abilities[1].ability.name
-  cardImg.src = "./assets/charmander.png"
-  card.style.background = 'linear-gradient(to top, #fff8bf 0% 50%, #F68C00 50% 100%)'
-}
+btnBulbasaur.addEventListener("click", () => renderPokemon(1, 'bulbasaur.png', '#59bc38'))
+btnCharmander.addEventListener("click", () => renderPokemon(4, 'charmander.png', ' #F68C00'))
+btnDragonite.addEventListener("click", () => renderPokemon(149, 'dragonite.png', '#FABE00' ))
+btnGyarados.addEventListener("click", () => renderPokemon(130, 'gyarados.png','#3DD4FB' ))
+btnPikachu.addEventListener("click", () => renderPokemon(25, 'pikachu.png', '#FFFC22' ))
 
-const renderDragonite = async (pokemon) => {
-  const data = await fetchPokemon(pokemon)
-
-  pokemonName.innerHTML = data.name
-  pokemonId.innerHTML = data.id
-  pokemonType.innerHTML = data.types[0].type.name
-  pokemonXp.innerHTML = `XP: ${data.base_experience}`
-  pokemonHabilities.innerHTML = data.abilities[1].ability.name
-  cardImg.src = "./assets/dragonite.png"
-  card.style.background = 'linear-gradient(to top, #fff8bf 0% 50%, #FABE00 50% 100%)'
-}
-
-const renderGyarados = async (pokemon) => {
-  const data = await fetchPokemon(pokemon)
-
-  pokemonName.innerHTML = data.name
-  pokemonId.innerHTML = data.id
-  pokemonType.innerHTML = data.types[0].type.name
-  pokemonXp.innerHTML = `XP: ${data.base_experience}`
-  pokemonHabilities.innerHTML = data.abilities[1].ability.name
-  cardImg.src = "./assets/gyarados.png"
-  card.style.background = 'linear-gradient(to top, #fff8bf 0% 50%, #3DD4FB 50% 100%)'
-}
-
-const renderPikachu = async (pokemon) => {
-  const data = await fetchPokemon(pokemon)
-
-  pokemonName.innerHTML = data.name
-  pokemonId.innerHTML = data.id
-  pokemonType.innerHTML = data.types[0].type.name
-  pokemonXp.innerHTML = `XP: ${data.base_experience}`
-  pokemonHabilities.innerHTML = data.abilities[1].ability.name
-  cardImg.src = "./assets/pikachu.png"
-  card.style.background = 'linear-gradient(to top, #fff8bf 0% 50%, #FFFC22 50% 100%)'
-}
-
-btnBulbasaur.addEventListener("click", () => renderBulbasaur(1))
-btnCharmander.addEventListener("click", () => renderCharmander(4))
-btnDragonite.addEventListener("click", () => renderDragonite(149))
-btnGyarados.addEventListener("click", () => renderGyarados(130))
-btnPikachu.addEventListener("click", () => renderPikachu(25))
+const imgAtualizar = document.querySelector('#atualizar')
 
 
-
-btnBulbasaur.addEventListener("click", function() {
-  cardBack.style.display === 'none' ? cardPrincipal.style.display = 'block' : cardBack.style.display = 'none'
+imgAtualizar.addEventListener('click', () => {
+  location.reload()
 })
 
-btnCharmander.addEventListener("click", function() {
-  cardBack.style.display === 'none' ? cardPrincipal.style.display = 'block' : cardBack.style.display = 'none'
+
+btnBulbasaur.addEventListener("click", () => {
+  cardBack.style.display === 'none' ? cardBack.style.display = 'block' : cardBack.style.display = 'none'
+  cardPrincipal.style.display === 'block' ? cardPrincipal.style.display = 'none' : cardPrincipal.style.display = 'block'
 })
 
-btnDragonite.addEventListener("click", function() {
-  cardBack.style.display === 'none' ? cardPrincipal.style.display = 'block' : cardBack.style.display = 'none'
+btnCharmander.addEventListener("click", () => {
+  cardBack.style.display === 'none' ? cardBack.style.display = 'block' : cardBack.style.display = 'none'
+  cardPrincipal.style.display === 'block' ? cardPrincipal.style.display = 'none' : cardPrincipal.style.display = 'block'
 })
 
-btnGyarados.addEventListener("click", function() {
-  cardBack.style.display === 'none' ? cardPrincipal.style.display = 'block' : cardBack.style.display = 'none'
+btnDragonite.addEventListener("click", () => {
+  cardBack.style.display === 'none' ? cardBack.style.display = 'block' : cardBack.style.display = 'none'
+  cardPrincipal.style.display === 'block' ? cardPrincipal.style.display = 'none' : cardPrincipal.style.display = 'block'
 })
 
-btnPikachu.addEventListener("click", function() {
-  cardBack.style.display === 'none' ? cardPrincipal.style.display = 'block' : cardBack.style.display = 'none'
+btnGyarados.addEventListener("click", () => {
+  cardBack.style.display === 'none' ? cardBack.style.display = 'block' : cardBack.style.display = 'none'
+  cardPrincipal.style.display === 'block' ? cardPrincipal.style.display = 'none' : cardPrincipal.style.display = 'block'
+})
+
+btnPikachu.addEventListener("click", () => {
+  cardBack.style.display === 'none' ? cardBack.style.display = 'block' : cardBack.style.display = 'none'
+  cardPrincipal.style.display === 'block' ? cardPrincipal.style.display = 'none' : cardPrincipal.style.display = 'block'
 })
 
 
